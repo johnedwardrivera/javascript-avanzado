b63ec980ebc6a16327c7f1677f3518df7e086de9
johnedwardrivera <edwardrivera0294@gmail.com>
2022-12-10 15:55:51 -0500

    repositoria para aprender

---
+++ checkpoint/01.js
@@ -0,0 +1,50 @@
+const Queue = require("../DS").Queue;
+// ⚠️ NO MODIFICAR NADA POR ENCIMA DE ESTA LÍNEA ⚠️
+//
+//  ----*** 👔 CLOTHES CHECKPOINT 👔 ***----
+//
+// Felicitaciones por llegar a esta instancia! El checkpoint que realizarás a continuación
+// consistirá en resolver varios problemas que surgieron en una tienda de ropa/indumentaria.
+// Tendrás que resolver cada uno de ellos, aplicando los conceptos aprendidos durante el Módulo Uno.
+// 🍀 Suerte!!! 🍀
+//
+//
+// 1️⃣ ***** EJERCICIO 1 QUEUE ***** - guardarCamisetas 1️⃣
+//
+// Ha llegado un camión con un nuevo lote de camisetas, es necesario recoger todas y guardarlas en dentro de la tienda.
+// Para esto tendrás que implementar la función guardarCamisetas, la cuál recibirá por parámetros una queue con muchas
+// 👕 Camisetas 👕 que se tendrán que guardar en un objeto que nos servirá como contenedor.
+//
+// EJEMPLOS:
+// Dada la siguiente queue (INPUT):
+// ["Camiseta Blanca", "Camiseta Negra", "Camiseta Azul", "Camiseta Amarilla", "Camiseta Roja"]
+// guardarCamisetas() Devuelve (OUTPUT) ➡
+// {
+//  Camiseta Blanca: Camiseta Blanca,
+//  Camiseta Negra: Camiseta Negra,
+//  Camiseta Amarilla: Camiseta Amarilla,
+//  Camiseta Roja: Camiseta Roja
+// }
+//
+// REQUISITOS:
+//  🟢 Respetar el principio de FIFO que corresponde a las queues a la hora de guardar las camisetas
+//  🟢 Retornar un objeto, que tendrá como key value, los elementos que agregas y quitas de la queue
+//  🟢 SI O SI necesitás utilizar una queue!
+// TIP: Chequear el archivo DS.js para ver la función constructora Queue, y ver sus métodos disponibles.
+
+function guardarCamisetas(ropaQueue) {
+  // Tu código aquí:
+  let camisas = {}; // guardala en un objeto que sirve como contenedor
+  let size = ropaQueue.size();
+  for (let index = 0; index < size; index++) {
+    let one = ropaQueue.dequeue();
+    camisas[one] = one;
+  }
+
+  return camisas;
+}
+
+// ⚠️ NO MODIFICAR NADA POR DEBAJO DE ESTA LÍNEA ⚠️
+module.exports = {
+  guardarCamisetas,
+};
